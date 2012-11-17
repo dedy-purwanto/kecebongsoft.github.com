07e6eba838edfd3c129d0bfcf575da778c9042f3
Dedi Purwanto <kecebongsoft@gmail.com>
2012-11-17 16:56:03 +0800

    Moved add new build mechanics

--- pelican/build.sh
+++ pelican/build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-ls ../ | grep -v README | grep -v pelican | grep -v CNAME | grep -v static | xargs -i rm -rf ../{}
+GITMODULES=`grep path ../.gitmodules | sed 's/path = /\\\|/g' | sed 's/ //g' | sed 's/\t//g' | tr '\\n' '#' | sed 's/#//g' | sed 's/^\\\|//g'`
+FILES='README\|pelican\|CNAME\|static'
+EXCEPTIONS=$GITMODULES'\|'$FILES
+ls ../ | grep -v $EXCEPTIONS | xargs -i rm -rf ../{}
+source env/bin/activate
 pelican . -o ../ -s pelican.conf.py
