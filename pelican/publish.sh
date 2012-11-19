69b7e2a9760b904cd7073eb68041c6c7f0ec82d6
Dedi Purwanto <kecebongsoft@gmail.com>
2012-11-19 19:37:12 +0800

    Added slide-opensource module, pushed external path to remote s3

--- pelican/publish.sh
+++ pelican/publish.sh
@@ -3,5 +3,6 @@ source ~/myaws_vars
 source env/bin/activate
 BUCKET='kecebongsoft'
 boto-rsync -g public-read ../build/ s3://kecebongsoft/blog/
+boto-rsync -g public-read ../externals/* s3://kecebongsoft/blog/
 
 
