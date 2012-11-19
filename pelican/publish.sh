b239192d6f5fb5628705c33fcf78a0ad36cf9700
Dedi Purwanto <kecebongsoft@gmail.com>
2012-11-19 20:02:12 +0800

    update to new remote bucket

--- pelican/publish.sh
+++ pelican/publish.sh
@@ -2,7 +2,7 @@
 source ~/myaws_vars
 source env/bin/activate
 BUCKET='kecebongsoft'
-boto-rsync -g public-read ../build/ s3://kecebongsoft/blog/
-boto-rsync -g public-read ../externals/ s3://kecebongsoft/blog/
+boto-rsync -g public-read ../build/ s3://kecebongsoft-blog/
+boto-rsync -g public-read ../externals/ s3://kecebongsoft-blog/
 
 
