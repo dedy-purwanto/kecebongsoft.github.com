05eef2719a75cd1244416863c420bb28f4a40564
Dedi Purwanto <kecebongsoft@gmail.com>
2012-11-20 11:13:49 +0800

    Updated publish script

--- pelican/publish.sh
+++ pelican/publish.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-source ~/myaws_vars
-source env/bin/activate
-BUCKET='kecebongsoft'
-boto-rsync -g public-read ../build/ s3://kecebongsoft-blog/
-boto-rsync -g public-read ../externals/ s3://kecebongsoft-blog/
-
-
+rsync -r ../build/* kecebongsoft@web321.webfaction.com:/home/kecebongsoft/webapps/blog/
+rsync -r ../externals/* kecebongsoft@web321.webfaction.com:/home/kecebongsoft/webapps/blog/
