34d570fa4a27bfcd519761c07bbb77adebad1820
Dedi Purwanto <kecebongsoft@gmail.com>
2013-12-10 11:40:21 +0800

    Push for every publish

--- pelican/publish.sh
+++ pelican/publish.sh
@@ -2,3 +2,4 @@
 ./build.sh
 rsync -v -r ../build/* kecebongsoft@web321.webfaction.com:/home/kecebongsoft/webapps/blog/
 rsync -v -r ../externals/* kecebongsoft@web321.webfaction.com:/home/kecebongsoft/webapps/blog/
+git push
