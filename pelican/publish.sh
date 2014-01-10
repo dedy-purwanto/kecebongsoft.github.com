7e1e9cc2271eca70c0065c78c90b1b749b1d72e7
Dedi Purwanto <kecebongsoft@gmail.com>
2014-01-10 15:26:55 +0800

    Removed all files

--- pelican/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-./build.sh
-rsync -v -r ../build/* kecebongsoft@web321.webfaction.com:/home/kecebongsoft/webapps/blog/
-rsync -v -r ../externals/* kecebongsoft@web321.webfaction.com:/home/kecebongsoft/webapps/blog/
-git push
